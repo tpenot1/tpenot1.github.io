95a41d12d42407228af073fbaabed5dff738ddeb
tpenot1 <35709787+tpenot1@users.noreply.github.com>
2018-02-19 18:50:38 -0600

    1

--- Portfolio/code-assignment-03.js
+++ Portfolio/code-assignment-03.js
@@ -1,2 +1,11 @@
-let x = "hello"
-console.log(x)
+let projectId = 1
+let projectTitle = "Map 1"
+let projectUrl = "file:///C:/Users/tpenot1/Documents/GitHub/tpenot1.github.io/Portfolio/index.html"
+let hasThumbnail = true
+let projectKeywords = ['web gis','GEOG 4046']
+
+console.log(projectId)
+console.log(projectTitle)
+console.log(projectUrl)
+console.log(hasThumbnail)
+console.log(projectKeywords)
