0ec81507f760c79ef002225fc4a1a517f0240f53
tpenot1 <35709787+tpenot1@users.noreply.github.com>
2018-02-26 15:06:45 -0600

    1

--- Portfolio/code-assignment-03.js
+++ Portfolio/code-assignment-03.js
@@ -1,8 +1,20 @@
-let projectId = 1
+let project1Id = 1
 let projectTitle = "Story Map of Arlington Virginia"
 let projectUrl = "https://www.arcgis.com/apps/MapTour/index.html?appid=58b54e7b9cfd45498d2c8ae68ee80dde"
 let hasThumbnail = true
 let projectKeywords = ['web gis','GEOG 4046','book assignment 1']
+
+console.log(projectId)
+console.log(projectTitle)
+console.log(projectUrl)
+console.log(hasThumbnail)
+console.log(projectKeywords)
+
+let project2Id = 2
+let projectTitle = "Real-time earthquakes map"
+let projectUrl = "http://www.arcgis.com/apps/View/index.html?appid=99f1593a2a3841d29dd76e070e34bed2"
+let hasThumbnail = true
+let projectKeywords = ['web gis','GEOG 4046','book assignment 2']
 
 console.log(projectId)
 console.log(projectTitle)
