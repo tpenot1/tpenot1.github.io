9d9ba750a33b6de874f42b5dba1b32d674dae87c
tpenot1 <35709787+tpenot1@users.noreply.github.com>
2018-02-26 15:21:04 -0600

    1

--- Portfolio/code-assignment-03.js
+++ Portfolio/code-assignment-03.js
@@ -1,23 +1,23 @@
 let project1Id = 1
-let projectTitle = "Story Map of Arlington Virginia"
-let projectUrl = "https://www.arcgis.com/apps/MapTour/index.html?appid=58b54e7b9cfd45498d2c8ae68ee80dde"
-let hasThumbnail = true
-let projectKeywords = ['web gis','GEOG 4046','book assignment 1']
+let project1Title = "Story Map of Arlington Virginia"
+let project1Url = "https://www.arcgis.com/apps/MapTour/index.html?appid=58b54e7b9cfd45498d2c8ae68ee80dde"
+let hasThumbnail1 = true
+let project1Keywords = ['web gis','GEOG 4046','book assignment 1']
 
-console.log(projectId)
-console.log(projectTitle)
-console.log(projectUrl)
-console.log(hasThumbnail)
-console.log(projectKeywords)
+console.log(project1Id)
+console.log(project1Title)
+console.log(project1Url)
+console.log(hasThumbnail1)
+console.log(project1Keywords)
 
 let project2Id = 2
-let projectTitle = "Real-time earthquakes map"
-let projectUrl = "http://www.arcgis.com/apps/View/index.html?appid=99f1593a2a3841d29dd76e070e34bed2"
-let hasThumbnail = true
-let projectKeywords = ['web gis','GEOG 4046','book assignment 2']
+let project2Title = "Real-time earthquakes map"
+let project2Url = "http://www.arcgis.com/apps/View/index.html?appid=99f1593a2a3841d29dd76e070e34bed2"
+let hasThumbnail2 = true
+let project2Keywords = ['web gis','GEOG 4046','book assignment 2']
 
-console.log(projectId)
-console.log(projectTitle)
-console.log(projectUrl)
-console.log(hasThumbnail)
-console.log(projectKeywords)
+console.log(project2Id)
+console.log(project2Title)
+console.log(project2Url)
+console.log(hasThumbnail2)
+console.log(project2Keywords)
