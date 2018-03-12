87f56ae23b1b73824349dc1ba251029498c5a694
tpenot1 <35709787+tpenot1@users.noreply.github.com>
2018-03-12 17:35:42 -0500

    update

--- code-assignment-05.js
+++ code-assignment-05.js
@@ -1,6 +1,6 @@
 let projects = []
 
-projects [0] = {
+project[0] = {
   id: 1,
   url: 'https://www.arcgis.com/apps/MapTour/index.html?appid=58b54e7b9cfd45498d2c8ae68ee80dde',
   title: 'Story Map of Arlington Virginia',
@@ -8,7 +8,7 @@ projects [0] = {
   keywords: ['story map','Arlington','Virginia','book assignment 1',]
 }
 
-projects[1] = {
+project[1]= {
   id: 2,
   url: 'http://www.arcgis.com/apps/View/index.html?appid=99f1593a2a3841d29dd76e070e34bed2',
   title: "Real-time earthquakes map",
