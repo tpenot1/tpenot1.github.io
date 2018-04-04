1f7fb2d1f10d4d11a2109d51e2d2d7392cbef9bd
tpenot1 <35709787+tpenot1@users.noreply.github.com>
2018-04-04 14:57:22 -0500

    update

--- map/webmap-assignment-03.js
+++ map/webmap-assignment-03.js
@@ -1,33 +1,36 @@
 let mymap = L.map('webmap3').setView([30.411701, -91.184524], 12)
 
-let url = ('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}')
-L.tileLayer(url).addTo(mymap)
+let.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
 
-let pick = L.icon({
-  iconUrl:
-  shadowUrl:
-  iconSize:
-  shadowSize:
-  iconAnchor:
-  shadowAnchor:
-  popupAnchor:
+let greenIcon = L.icon({
+  iconUrl: 'images/leaf-green.png',
+  shadowUrl: 'images/leaf-green.png',
+  iconSize: [38, 95],
+  shadowSize: [50, 64],
+  iconAnchor: [22, 94],
+  shadowAnchor: [4, 62]
+  popupAnchor: [-3, -76]
 })
 
-let markerCoords = [30.411701, -91.184524]
-let markerOptions = {
-  icon: pick
-}
-let station = L.marker(markerCoords, markerOptions).addTo(mymap)
+let marker = L.marker([30.411701, -91.184524], { icon: greenIcon }).addTo(mymap)
 
-let polygonCoords = [
+let polygonCorners = [
   [30.418511, -91.193793],
   [30.41977, -91.177256],
   [30.38919, -91.189916]
 ]
 
 let polygonStyle = {
-  color: 'orange',
-  fullColor: 'yellow',
-  weight: 1,
-  dashArray: 3
+  color: 'blue',
+  fillColor: 'red'
+}
+
+let polygon = L.polygon(polygonCorners, polygonStyle).addTo(mymap)
+
+polygon.bindPopup('Baton Rouge, LA - Louisiana State University')
+marker.bindPopup('LSU and surrounding roads - River Rd. to Highland Rd. to Brightside Ln.')
+
+mymap.on('click', function (event) {
+  console.log('You clicked the map at ' + event.latlng)
+  })
 }
