244308877e4f794b92d0fb47aabc0aafc100eaee
tpenot1 <35709787+tpenot1@users.noreply.github.com>
2018-04-09 12:48:11 -0500

    update

--- map/webmap-assignment-03.js
+++ map/webmap-assignment-03.js
@@ -2,14 +2,17 @@ let mymap = L.map('webmap3').setView([30.411701, -91.184524], 12)
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
 
-let pick = L.icon({
+let myGreenIcon = L.icon({
   iconUrl: 'map/leaf-green.png',
-  iconSize: [43, 50],
-  iconAnchor: [43, 50],
-  popupAnchor: [-22, -27]
+  shadowUrl: 'map/leaf-shadow.png',
+  iconSize: [38, 95],
+  shadowSize: [50, 64],
+  iconAnchor: [22, 94],
+  shadowAnchor: [4, 62],
+  popupAnchor: [-3, -76]
 })
 
-let marker = L.marker([30.411701, -91.184524], { icon: pick }).addTo(mymap)
+let marker = L.marker([30.411701, -91.184524], { icon: myGreenIcon }).addTo(mymap)
 
 let polygonCorners = [
   [30.418511, -91.193793],
